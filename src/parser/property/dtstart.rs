8c6567cbe5beb11d981d0520f727a15795bb8b70
Lennart K <18233294+lennart-k@users.noreply.github.com>
2026-01-05 21:12:41 +0100

    DTSTART: Add local test case

--- src/parser/property/dtstart.rs
+++ src/parser/property/dtstart.rs
@@ -25,6 +25,7 @@ mod tests {
     #[rstest]
     #[case("DTSTART:19980118T073000Z\r\n")]
     #[case("DTSTART;TZID=Europe/Berlin:19980118T073000Z\r\n")]
+    #[case("DTSTART;TZID=W. Europe Standard Time:20210527T120000\r\n")]
     fn roundtrip(#[case] input: &str) {
         let content_line = crate::PropertyParser::from_reader(input.as_bytes())
             .next()
@@ -32,6 +33,7 @@ mod tests {
             .unwrap();
         let mut timezones = HashMap::new();
         timezones.insert("Europe/Berlin".to_owned(), Some(chrono_tz::Europe::Berlin));
+        timezones.insert("W. Europe Standard Time".to_owned(), None);
         let prop = IcalDTSTARTProperty::parse_prop(&content_line, &timezones).unwrap();
         let roundtrip: ContentLine = prop.into();
         similar_asserts::assert_eq!(roundtrip.generate(), input);
