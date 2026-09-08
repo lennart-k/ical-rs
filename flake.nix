1b29b741ec0c49f9d58b4c448c30ef1f3a4aeef2
Lennart Kämmle <18233294+lennart-k@users.noreply.github.com>
2026-09-08 18:32:42 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment for caldata-rs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # The task runner, similar to make
+            just
+
+            cargo-vet
+            cargo-about
+            cargo-deny
+          ];
+        };
+      }
+    );
+}
